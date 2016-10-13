7a4e1964b34813a3d0f40a784941bc038ab12ebf
sernacalle <sernacalle@hotmail.com>
2016-10-12 20:45:40 -0500

    View
    View

--- MVC/Vista/usuarios_view.php
+++ MVC/Vista/usuarios_view.php
@@ -3,8 +3,28 @@
 <head>
 <meta charset="utf-8">
 <title>Documento sin título</title>
+<style>
+td {
+	border:1px dotted #060;
+	}
+
+</style>
+
+
+
 </head>
 
 <body>
+<table>
+<tr><td>Nombre del usuario</td></tr>
+<?php
+
+	foreach($matrizUsuario as $registro){
+		
+		echo "<tr><td>". $registro["Usuarios"] . "</tr></td>";
+		}
+
+?>
+</table>
 </body>
 </html>
