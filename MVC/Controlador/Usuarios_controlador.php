a1a68b87134b775e3b10dda67e677c208c52fb2a
sernacalle <sernacalle@hotmail.com>
2016-10-12 20:36:04 -0500

    controlador
    controlador

--- MVC/Controlador/Usuarios_controlador.php
+++ MVC/Controlador/Usuarios_controlador.php
@@ -1,6 +1,13 @@
 <?php
 
-
+	require_once ("../modelo/usuarios_modelo.php");
+	
+	$usuario= new Usuarios_modelo();
+	$matrizUsuarios=$usuario->get_usuarios();
+	
+	
+	
+	require_once ("../vista/usuarios_view.php");
 
 
 ?>
