82def21058be673fb68012cae83f303ae0dc549f
sernacalle <sernacalle@hotmail.com>
2016-10-16 11:03:28 -0500

    Cifrado Login
    cifrado

--- Practica/comprueba_login.php
+++ Practica/comprueba_login.php
@@ -1,17 +1,26 @@
 <?php
 
 try {
+		$login=htmlentities(addslashes($_POST["login"]));
+		$password=htmlentities(addslashes($_POST["password"]));
+		
 		$base=new PDO ("mysql:host =localhost; dbname=pruebas", "root", "");
 		$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql="SELECT * FROM USUARIOS WHERE USUARIOS= :login AND PASSWORD= :password";
-		$resultado=$base->prepare($sql);
-		$login=htmlentities (addslashes($_POST["login"]));
-		$password=htmlentities(addslashes($_POST["password"]));
-		$resultado->bindValue(":login", $login);
-		$resultado->bindValue(":password", $password);
-		$resultado->execute();
-		$numero_registro=$resultado->rowCount();
+		$sql="SELECT * FROM USUARIOS WHERE USUARIOS= :login";
+	
 		
+			
+		
+		$resultado=$base->prepare($sql);
+		$resultado->execute(array(":login"=>$login));
+			while($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
+				
+					if(password_verify($password, $registro['PASSWORD']));{
+				$numero_registro=$resultado->rowCount();
+			}
+			
+		
+		}
 		
 		
 		if ($numero_registro!=0){
